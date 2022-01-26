bf10a042b4f69249193fe9c61895342dec7046c0
최시열 <continue75@naver.com>
2022-01-26 11:02:17 +0900

    initial commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>안녕, 세상아</h1>
+    <p>CI/CD 성공!!!!!!!!!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+p {
+  color: #42b983;
+}
+</style>
