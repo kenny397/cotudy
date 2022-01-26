b6f57a11c5aa84c364d5658779ff7064e04f07a3
yunoh <orobot13@gmail.com>
2022-01-26 10:50:04 +0900

    init

---
+++ a105/a105/src/main/java/com/ssafy/a105/A105Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.a105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A105Application.class, args);
+	}
+
+}
