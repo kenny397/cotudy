9cc5d5bb94d86fd589eeb78a53d0db22fe1f72c6
서형준 <kenny397@naver.com>
2022-02-04 01:07:28 +0000

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -41,7 +41,7 @@ import UserVideo from './components/UserVideo';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const OPENVIDU_SERVER_URL = "https://i6b105.p.ssafy.io:4443";
+const OPENVIDU_SERVER_URL = "https://i6b105.p.ssafy.io:5442";
 const OPENVIDU_SERVER_SECRET = "MY_SECRET";
 
 export default {
