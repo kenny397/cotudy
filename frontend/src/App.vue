8b61207e93f5a24f9df424d2c89a04ad928976c9
서형준 <kenny397@naver.com>
2022-02-04 09:28:11 +0000

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -41,7 +41,7 @@ import axios from 'axios';
 import { OpenVidu } from 'openvidu-browser';
 import UserVideo from './components/UserVideo';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-const OPENVIDU_SERVER_URL = "https:i6b105.p.ssafy.io:5443";
+const OPENVIDU_SERVER_URL = "https:i6b105.p.ssafy.io:8443";
 const OPENVIDU_SERVER_SECRET = "MY_SECRET";
 export default {
 	name: 'App',
