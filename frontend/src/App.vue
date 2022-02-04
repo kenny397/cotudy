58e106937a03f5a3ebed7d112fb570070186bbdf
서형준 <kenny397@naver.com>
2022-02-04 08:27:06 +0000

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,7 @@
 		<div id="join" v-if="!session">
 			<div id="img-div"><img src="resources/images/openvidu_grey_bg_transp_cropped.png" /></div>
 			<div id="join-dialog" class="jumbotron vertical-center">
-				<h1>Join a video cicdsession</h1>
-				<video autoplay/>
+				<h1>Join a video session</h1>
 				<div class="form-group">
 					<p>
 						<label>Participant</label>
@@ -26,9 +25,12 @@
 				<h1 id="session-title">{{ mySessionId }}</h1>
 				<input class="btn btn-large btn-danger" type="button" id="buttonLeaveSession" @click="leaveSession" value="Leave session">
 			</div>
+			<div id="main-video" class="col-md-6">
+				<user-video :stream-manager="mainStreamManager"/>
+			</div>
 			<div id="video-container" class="col-md-6">
-				<user-video @inputUpdate="inputChange" :stream-manager="publisher" :session="session" @click.native="updateMainVideoStreamManager(publisher)"/>
-				<user-video @inputUpdate="inputChange" v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :session="session" :stream-manager="sub" @click.native="updateMainVideoStreamManager(sub)"/>
+				<user-video :stream-manager="publisher" @click.native="updateMainVideoStreamManager(publisher)"/>
+				<user-video v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub" @click.native="updateMainVideoStreamManager(sub)"/>
 			</div>
 		</div>
 	</div>
@@ -38,19 +40,14 @@
 import axios from 'axios';
 import { OpenVidu } from 'openvidu-browser';
 import UserVideo from './components/UserVideo';
-
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-const OPENVIDU_SERVER_URL = "https://i6b105.p.ssafy.io";
+const OPENVIDU_SERVER_URL = "https://http://i6b105.p.ssafy.io/";
 const OPENVIDU_SERVER_SECRET = "MY_SECRET";
-
 export default {
 	name: 'App',
-
 	components: {
 		UserVideo,
 	},
-
 	data () {
 		return {
 			OV: undefined,
@@ -60,53 +57,20 @@ export default {
 			subscribers: [],
 			mySessionId: 'SessionA',
 			myUserName: 'Participant' + Math.floor(Math.random() * 100),
-			myInput: '',
 		}
 	},
-
 	methods: {
-
-		inputChange(myInput) {
-			this.myInput = myInput
-		},
-
-		sendMessage () {
-			this.session.signal({
-      data: this.myInput,
-      to: [],  
-      type: 'my-chat' 
-			})
-			.then(() => {
-					console.log('Message successfully sent');
-			})
-			.catch(error => {
-					console.error(error);
-			})
-		},
-
-		sendedMessage () {
-			this.session.on('signal:my-chat', (event) => {
-				this.fromHigh.data = event.data
-				this.fromHigh.from = event.from.connectionId
-			});
-		},
-
 		joinSession () {
 			// --- Get an OpenVidu object ---
-			
 			this.OV = new OpenVidu();
-
 			// --- Init a session ---
 			this.session = this.OV.initSession();
-
 			// --- Specify the actions when events take place in the session ---
-
 			// On every new Stream received...
 			this.session.on('streamCreated', ({ stream }) => {
 				const subscriber = this.session.subscribe(stream);
 				this.subscribers.push(subscriber);
 			});
-
 			// On every Stream destroyed...
 			this.session.on('streamDestroyed', ({ stream }) => {
 				const index = this.subscribers.indexOf(stream.streamManager, 0);
@@ -114,24 +78,17 @@ export default {
 					this.subscribers.splice(index, 1);
 				}
 			});
-
 			// On every asynchronous exception...
 			this.session.on('exception', ({ exception }) => {
 				console.warn(exception);
 			});
-			//여기
-
-
 			// --- Connect to the session with a valid user token ---
-
 			// 'getToken' method is simulating what your server-side should do.
 			// 'token' parameter should be retrieved and returned by your own backend
 			this.getToken(this.mySessionId).then(token => {
 				this.session.connect(token, { clientData: this.myUserName })
 					.then(() => {
-
 						// --- Get your own camera stream with the desired properties ---
-
 						let publisher = this.OV.initPublisher(undefined, {
 							audioSource: undefined, // The source of audio. If undefined default microphone
 							videoSource: undefined, // The source of video. If undefined default webcam
@@ -142,43 +99,31 @@ export default {
 							insertMode: 'APPEND',	// How the video is inserted in the target element 'video-container'
 							mirror: false       	// Whether to mirror your local video or not
 						});
-
 						this.mainStreamManager = publisher;
 						this.publisher = publisher;
-
 						// --- Publish your stream ---
-
 						this.session.publish(this.publisher);
 					})
 					.catch(error => {
 						console.log('There was an error connecting to the session:', error.code, error.message);
 					});
 			});
-
-			// 여기
-			setInterval(this.sendMessage, 1000);
-
 			window.addEventListener('beforeunload', this.leaveSession)
 		},
-
 		leaveSession () {
 			// --- Leave the session by calling 'disconnect' method over the Session object ---
 			if (this.session) this.session.disconnect();
-
 			this.session = undefined;
 			this.mainStreamManager = undefined;
 			this.publisher = undefined;
 			this.subscribers = [];
 			this.OV = undefined;
-
 			window.removeEventListener('beforeunload', this.leaveSession);
 		},
-
 		updateMainVideoStreamManager (stream) {
 			if (this.mainStreamManager === stream) return;
 			this.mainStreamManager = stream;
 		},
-
 		/**
 		 * --------------------------
 		 * SERVER-SIDE RESPONSIBILITY
@@ -190,12 +135,10 @@ export default {
 		 *   2) Create a Connection in OpenVidu Server (POST /openvidu/api/sessions/<SESSION_ID>/connection)
 		 *   3) The Connection.token must be consumed in Session.connect() method
 		 */
-
 		getToken (mySessionId) {
 			return this.createSession(mySessionId).then(sessionId => this.createToken(sessionId));
 		},
-
-		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-openviduapisessions
+		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-session
 		createSession (sessionId) {
 			return new Promise((resolve, reject) => {
 				axios
@@ -222,8 +165,7 @@ export default {
 					});
 			});
 		},
-
-		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-openviduapisessionsltsession_idgtconnection
+		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-connection
 		createToken (sessionId) {
 			return new Promise((resolve, reject) => {
 				axios
