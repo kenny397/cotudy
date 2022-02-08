a5bb4bb0bddd4653c36ec4bf8438acb6b1d0c57b
Leo <guswhddlf11@gmail.com>
2022-02-08 10:14:11 +0900

    fix: eslint 문법 수정

--- frontend/src/views/myPage/myPage.vue
+++ frontend/src/views/myPage/myPage.vue
@@ -6,9 +6,9 @@
   <el-calendar v-model="value" />
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
+<script>
+// import { ref } from 'vue'
 
-const value = ref(new Date())
+// const value = ref(new Date())
 
 </script>
