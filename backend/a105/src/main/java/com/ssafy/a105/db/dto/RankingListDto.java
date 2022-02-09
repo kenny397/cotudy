03fd871c1d026b950f5aaaa81aa47530b52158bf
yunoh <orobot13@gmail.com>
2022-02-09 18:14:25 +0900

    feat : RankingListDto 추가

---
+++ backend/a105/src/main/java/com/ssafy/a105/db/dto/RankingListDto.java
@@ -0,0 +1,28 @@
+package com.ssafy.a105.db.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class RankingListDto {
+
+    String keyDepartment;
+    String keyPeriod;
+    String keyCategory;
+    String userNickname;
+
+    public static RankingListDto of(String keyDepartment,
+                                    String keyPeriod,
+                                    String keyCategory,
+                                    String userNickname){
+        RankingListDto body = new RankingListDto();
+        body.keyDepartment = keyDepartment;
+        body.keyPeriod = keyPeriod;
+        body.keyCategory = keyCategory;
+        body.userNickname = userNickname;
+        return body;
+    }
+}
