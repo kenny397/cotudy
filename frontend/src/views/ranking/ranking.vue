d67bf97b4eb058b5660d14d06ad3d61ff62a7a09
전영서 <hakkaame626@gmail.com>
2022-02-10 17:34:45 +0900

    Revert "Revert "랭킹페이지 수정""
    This reverts commit 3ef3219374e1ab77c843e9ae0f6b0c4aaae903f4.

--- frontend/src/views/ranking/ranking.vue
+++ frontend/src/views/ranking/ranking.vue
@@ -3,136 +3,142 @@
       <h2 style="text-align:center">로그인 하시면 나의 공부 정보를 볼 수 있어요!</h2>
   </div>
   <div style="display:flex; justify-content:space-around; align-items:center;">
-    <h3>Study List</h3>
+    <h3>종합 랭킹</h3>
     <h3/><h3/><h3/>
   </div>
-  <hr width = "80%"/>
-  <div style="display:flex; justify-content:space-around; align-items:center;">
-    <form action="#" method="post">
-      <div>
-      <el-select v-model="day" class="m-2" placeholder="한달">
-        <el-option
-          v-for="day in days"
-          :key="day.value"
-          :label="day.label"
-          :value="day.value"
-        >
-        </el-option>
-      </el-select>
-        <el-select v-model="group" class="m-2" placeholder="전체">
-        <el-option
-          v-for="group in groups"
-          :key="group.value"
-          :label="group.label"
-          :value="group.value"
-        >
-        </el-option>
-      </el-select>
-      </div>
-      <div>
-      <el-input
-          v-model="search"
-          class="m-2"
-          placeholder="아이디 입력"
-          :suffix-icon="Search"
-      ></el-input>
-      </div>
-       <el-button plain type="success">검색</el-button>
+  <hr width = "90%"/>
+  <div>
+    <el-row>
+      <el-col :span="2"/>
+    <el-col :span="2"> <el-select v-model="term" class="m-2" placeholder="전체">
+    <el-option
+      v-for="item in terms"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    >
+    </el-option>
+  </el-select>
+  </el-col>
+  <el-col :span="1"/>
+    <el-col :span="2">    <el-select v-model="class" class="m-2" placeholder="전체">
+    <el-option
+      v-for="item in classes"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    >
+    </el-option>
+  </el-select></el-col>
+<el-col :span="7"/>
+    <el-col :span="8">
+<div class="mt-4">
+    <el-input
+      v-model="input"
+      placeholder="검색어를 입력하세요"
+      class="input-with-select"
+    >
+      <template #prepend>
+        <el-select v-model="select" placeholder="아이디" style="width: 100px">
+          <el-option label="아이디" value="1"></el-option>
+          <el-option label="소속" value="2"></el-option>
+        </el-select>
+      </template>
+      <template #append>
+        <el-button :icon="Search"></el-button>
+      </template>
+    </el-input>
+  </div>
 
-    </form>
+    </el-col>
+  </el-row>
+
+  <el-table :data="rank" stripe border style="width: 100%" :row-class-name="tableRowClassName">
+    <el-table-column prop="#" label="" width="50" />
+    <el-table-column prop="index+1" label="순위" width="100" />
+    <el-table-column prop="nickName" label="닉네임" width="500" />
+    <el-table-column prop="department" label="소속" />
+    <el-table-column prop="totalStudyTime" label="공부한시간" />
+  </el-table>
 
 
   </div>
-<div style="display:flex; justify-content:space-around; align-items:center;">
 
-  <el-table :data="tableData"  height="250" stripe style="width: 100%" align="center">
-    <el-table-column prop="rank" label="랭크" width="180" />
-    <el-table-column prop="nickname" label="닉네임" width="180" />
-    <el-table-column prop="tear" label="티어" />
-    <el-table-column prop="group" label="소속" />
-    <el-table-column prop="time" label="공부한 시간" />
-  </el-table>
-
-</div>
-
-
-<div align="center">
-  <!--순위 많을때 번호...?-->
-</div>
 </template>
-
+  
 <script>
+import { Search } from '@element-plus/icons-vue'
+import axios from 'axios';
+export default {
+  name: 'Ranking',
 
-// export default {
-//   name: 'ranking',
-// }
+  components: {
 
-// import { ref } from 'vue'
-//왜 안되지...
-//import { Search } from '@element-plus/icons-vue'
-// const day = ref('')
-// const group = ref('')
-// const search = ref('')
-// const days = [
-//   {
-//     value: '1',
-//     label: '1일',
-//   },
-//   {
-//     value: '2',
-//     label: '7일',
-//   },
-//   {
-//     value: '3',
-//     label: '한달',
-//   },
-// ]
+  },
 
-// const groups = [
-//   {
-//     value: '1',
-//     label: '전체',
-//   },
-//   {
-//     value: '2',
-//     label: '내 소속',
-//   },
-//   {
-//     value: '3',
-//     label: '친구',
-//   },
-// ]
+  data(){
+    return {
+      turm : '',
+      terms : [{
+        value : 'whole',
+        label : '전체'
+      },{
+        value : 'day',
+        label : '1일'
+      },{
+        value : 'week',
+        label : '1주일'
+      },{
+        value : 'month',
+        label : '한달'
+      },],
+      class : '',
+      classes : [{
+        value : 'all',
+        label : '전체'
+      },{
+        value : 'department',
+        label : '내 소속'
+      },{
+        value : 'rival',
+        label : '친구'
+      },],
+
+      select : '',
+      conditions : [{
+        value : 'id',
+        label : '아이디'
+      },{
+        value : 'department',
+        label : '소속'
+      },],
+
+      rank : []
+    }
+  },
+
+  methods: {
+    fetchRank(){
+      axios.get('ranking').then((Response)=>{
+    this.rank = Response.data.content
+    }).catch((Error)=>{
+    console.log(Error);
+})
+    }
+  },
+
+  created(){
+    this.fetchRank();
+  },
 
 
-// const tableData = [
-//   {
-//     date: '2016-05-04',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-//   {
-//     date: '2016-05-01',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-//   {
-//     date: '2016-05-08',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-//   {
-//     date: '2016-05-06',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-//   {
-//     date: '2016-05-07',
-//     name: 'Tom',
-//     address: 'No. 189, Grove St, Los Angeles',
-//   },
-// ]
+}
 </script>
 
-<style>
 
+
+<style scoped>
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+}
 </style>
