451c63c398bdb63b86644b1e55dbbb2bca01409d
최시열 <continue75@naver.com>
2022-02-14 10:04:46 +0900

    fix: 메인 페이지 각종 버그 수정

--- frontend/src/views/myPage/myPage.vue
+++ frontend/src/views/myPage/myPage.vue
@@ -7,7 +7,31 @@
 </template>
 
 <script>
-// import { ref } from 'vue'
+import { onBeforeMount } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'mypage',
+
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+
+    onBeforeMount(() => {
+      isLogin()
+    })
+
+    const isLogin = function () {
+      if (!store.state['root'].isLogin) {
+        router.push({
+          name:'home'
+        })
+      }
+    }
+  }
+
+}
 
 // const value = ref(new Date())
 
